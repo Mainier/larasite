4f2bccd15f4e71413151a87a8f8635ed32c42cc9
Mainier <michaelmainier@Michaels-MacBook-Pro.local>
2014-02-18 15:05:40 -0500

    App Setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Larasite::Application.config.secret_key_base = '5626c815e0216dc0838e71471da53c2e9ac5b6e402459b34f6bb85daa06dafd4c60bd5bf76e9a873b18cfbfec529bad493832dff2e14068fb1a2238bd50297d9'
